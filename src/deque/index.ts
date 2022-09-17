b569fb87478e023ddb3dfa7f93a04a1d0016ada9
ZLY201 <951711127@qq.com>
2022-09-18 00:29:31 +0800

    feat: add deque memory test

--- src/deque/index.ts
+++ src/deque/index.ts
@@ -1,12 +1,14 @@
 import getTest from './get';
 import pushTest from './push';
+import memoryTest from './memory';
 
 export default async function () {
   console.info('deque testing...');
   const content = await Promise.all([
     '## deque\n',
     pushTest(),
-    getTest()
+    getTest(),
+    memoryTest()
   ]);
   console.info('deque test finished.');
   return content.join('\n');
---
+++ src/deque/memory.ts
@@ -0,0 +1,36 @@
+import { Deque } from 'js-sdsl';
+import Denque from 'denque';
+
+function catchMemory(Queue: typeof Deque | typeof Denque, size: number) {
+  const pre = process.memoryUsage().heapUsed;
+  const a = new Queue();
+  for (let i = 0; i < size; ++i) {
+    if (a instanceof Denque) a.push(i);
+    else if (a instanceof Deque) a.pushBack(i);
+  }
+  const now = process.memoryUsage().heapUsed;
+  return [a, now - pre] as [unknown, number];
+}
+
+export default function () {
+  console.log('deque memory testing...');
+  const testList = [1];
+  for (let i = 1; i <= 7; ++i) {
+    testList.push(testList[i - 1] * 10);
+  }
+  testList.shift();
+  let content =
+`### memory
+
+memory diff is \`Denque\` - \`Deque\`
+
+\`\`\`bash\n`;
+  content += testList.map(size => {
+    const [a, memoryA] = catchMemory(Deque, size);
+    const [b, memoryB] = catchMemory(Denque, size);
+    return [a, b, memoryB - memoryA] as [unknown, unknown, number];
+  }).map((arr, index) =>
+    `size: 10^${index + 1}    memory-diff: ${Math.floor(arr[2] / 1024)} KB`
+  ).join('\n');
+  return content + '\n```\n';
+}
