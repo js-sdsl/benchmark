783b41b973ca70bdb559177a759f70996985ceae
ZLY201 <951711127@qq.com>
2022-09-17 23:33:39 +0800

    feat: update rbtree insert bench

--- src/rbTree/insert.ts
+++ src/rbTree/insert.ts
@@ -11,26 +11,30 @@ export default async function () {
     arr.push(Math.random() * l * l);
   }
   const suite = new Benchmark.Suite();
+  const orderedMap = new OrderedMap<number, number>();
+  const binTree = new BinTree.RBTree<number>((x: number, y: number) => x - y);
+  let functionalRedBlackTree = FunctionalRedBlackTree<number, number>();
+  const map = new Map();
   suite.add('js-sdsl', function () {
-    const orderedMap = new OrderedMap<number, number>();
     for (let i = 0; i < l; ++i) {
       orderedMap.setElement(arr[i], i);
     }
+    orderedMap.clear();
   }).add('bintree', function () {
-    const binTree = new BinTree.RBTree<number>((x: number, y: number) => x - y);
     for (let i = 0; i < l; ++i) {
       binTree.insert(arr[i]);
     }
+    binTree.clear();
   }).add('functional-red-black-tree', function () {
-    let functionalRedBlackTree = FunctionalRedBlackTree<number, number>();
     for (let i = 0; i < l; ++i) {
       functionalRedBlackTree = functionalRedBlackTree.insert(arr[i], i);
     }
+    functionalRedBlackTree = FunctionalRedBlackTree<number, number>();
   }).add('es6-map', function () {
-    const map = new Map();
     for (let i = 0; i < l; ++i) {
       map.set(arr[i], i);
     }
+    map.clear();
   });
   return await new Promise<string>(resolve => run(suite, 'insert', resolve));
 }
