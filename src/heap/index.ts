94f6d586654a4816e62debdb9431f303daca45b4
ZLY201 <951711127@qq.com>
2023-01-17 11:44:14 +0800

    chore: upgrade dependencies

--- src/heap/index.ts
+++ src/heap/index.ts
@@ -1,14 +1,12 @@
 import testPush from './push';
 import testPop from './pop';
 import testInit from './init';
-import testCopyFalse from './copyFalse';
 
 export default async function () {
   console.info('heap testing...');
   const content = await Promise.all([
     '## heap\n',
     testInit(),
-    testCopyFalse(),
     testPush(),
     testPop()
   ]);
--- src/heap/init.ts
+++ src/heap/init.ts
@@ -7,31 +7,41 @@ import { MinHeap } from 'mnemonist';
 import { PriorityQueue as PriorityQueueCollections } from 'typescript-collections';
 
 export default async function () {
-  const l = 10000;
-  const arr: number[] = [];
-  for (let i = 0; i < l; ++i) arr.push(i);
+  const l = 1000;
   const suite = new Benchmark.Suite();
   suite.add('js-sdsl', function () {
+    const arr: number[] = [];
+    for (let i = 0; i < l; ++i) arr.push(i);
     // eslint-disable-next-line no-new
-    new PriorityQueue(arr, (x, y) => x - y);
+    new PriorityQueue(arr, (x, y) => x - y, false);
   }).add('heap', function () {
+    const arr: number[] = [];
+    for (let i = 0; i < l; ++i) arr.push(i);
     const heap = new Heap<number>((x, y) => x - y);
     for (let i = 0; i < l; ++i) {
       heap.push(arr[i]);
     }
   }).add('heap-js', function () {
+    const arr: number[] = [];
+    for (let i = 0; i < l; ++i) arr.push(i);
     const minHeap = new HeapJs<number>((x, y) => x - y);
     minHeap.init(arr);
   }).add('mnemonist', function () {
+    const arr: number[] = [];
+    for (let i = 0; i < l; ++i) arr.push(i);
     const minHeap = new MinHeap<number>((x, y) => x - y);
     for (let i = 0; i < l; ++i) {
       minHeap.push(arr[i]);
     }
   }).add('typescript-collections', function () {
+    const arr: number[] = [];
+    for (let i = 0; i < l; ++i) arr.push(i);
     const minHeap = new PriorityQueueCollections<number>((x, y) => x - y);
     for (let i = 0; i < l; ++i) {
       minHeap.add(i);
     }
   });
-  return await new Promise(resolve => run(suite, 'init heap in constructor', resolve));
+  return await new Promise(
+    resolve => run(suite, 'init heap in constructor when copy is false', resolve)
+  );
 }
